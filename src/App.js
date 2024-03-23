8841ecf2c66fd9cc7b66f5689e94d3851fc54442
Camilo Arboleda <camiloarboleda2000@gmail.com>
2024-03-23 16:47:30 -0500

    Inicializando proyecto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Game } from './views/game/Game';
+
+function App() {
+  return (
+    <div className="App">
+      <Game className="App" />
+    </div>
+  );
+}
+
+export default App;
